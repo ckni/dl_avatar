46ed67b9532a1219b97a5c4db37a1cf0dcf76892
CKNI <ckni@pm.me>
2018-06-09 11:27:37 -0700

    Updated main executable to use repository specified in parameters

--- dl_avatar.js
+++ dl_avatar.js
@@ -5,6 +5,7 @@ var auth = require("./secret.js");
 var user = process.argv[2];
 var repo = process.argv[3];
 
+// contact GitHub API to get data about members of a specified repository
 function getRepoContributors(user, repo, cb) {
 	var options = {
 		url: "https://api.github.com/repos/" + user + "/" + repo + "/contributors",
@@ -12,12 +13,13 @@ function getRepoContributors(user, repo, cb) {
 			"User-Agent": "request"
 		},
 		authorization: auth.GITHUB_TOKEN
-	}
+	};
 	request(options, function(err, res, body) {
 		cb(err, body);
 	});
 }
 
+// download an image given its URL to a specified directory
 function downloadImageByURL(url, filePath) {
 	request.get(url)
 		.on("error", function(err) {
@@ -29,16 +31,32 @@ function downloadImageByURL(url, filePath) {
 		.pipe(fs.createWriteStream(filePath));
 }
 
-mkdirp("./Avatars", function(err) {
-	if (err) {
-		console.log(err);
-	}
-});
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-	console.log("Errors:", err);
-	var json = JSON.parse(result);
-	json.forEach(function(e) {
-		downloadImageByURL(e.avatar_url, "./Avatars/" + e.login + ".jpg");
+// making sure that parameters are valid
+var usage = "Usage: node dl_avatar.js <repo owner> <repo name>";
+if (!user || !repo) {
+	// log error and terminate program
+	console.log("Error: Missing parameters\n" + usage);
+	process.exit();
+} else {
+	// make Avatars directory for storing downloaded images (if it does not already exist)
+	mkdirp("./Avatars", function(err) {
+		if (err) {
+			console.log(err);
+		}
 	});
-});
+	// get json from github api, extract avatar URLs, and download images to Avatars directory
+	getRepoContributors(user, repo, function(err, result) {
+		// download avatar of each user into Avatars directory
+		var json = JSON.parse(result);
+		// make sure result exists
+		if (json.message === "Not Found") {
+			// log message and terminate program
+			console.log("Error: Repository not found");
+			process.exit();
+		} else {
+			json.forEach(function(e) {
+				downloadImageByURL(e.avatar_url, "./Avatars/" + e.login + ".jpg");
+			});
+		}
+	});
+}
