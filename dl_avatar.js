a4cf9d7230e7120d96fade1b405499011a55df67
CKNI <ckni@pm.me>
2018-06-09 11:01:23 -0700

    Updated main executable to make Avatars directory using mkdirp and download avatars to it using fs

--- dl_avatar.js
+++ dl_avatar.js
@@ -1,4 +1,6 @@
 var request = require("request");
+var fs = require("fs");
+var mkdirp = require("mkdirp");
 var auth = require("./secret.js");
 var user = process.argv[2];
 var repo = process.argv[3];
@@ -16,10 +18,27 @@ function getRepoContributors(user, repo, cb) {
 	});
 }
 
+function downloadImageByURL(url, filePath) {
+	request.get(url)
+		.on("error", function(err) {
+			throw err;
+		})
+		.on("response", function(response) {
+			console.log("Response Status Code:", response.statusCode);
+		})
+		.pipe(fs.createWriteStream(filePath));
+}
+
+mkdirp("./Avatars", function(err) {
+	if (err) {
+		console.log(err);
+	}
+});
+
 getRepoContributors("jquery", "jquery", function(err, result) {
 	console.log("Errors:", err);
 	var json = JSON.parse(result);
 	json.forEach(function(e) {
-		console.log(e.avatar_url);
+		downloadImageByURL(e.avatar_url, "./Avatars/" + e.login + ".jpg");
 	});
 });
